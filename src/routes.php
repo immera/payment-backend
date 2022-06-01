aaf1e83a0a279602c70a4d0aa376fae91bc2d1f1
Harikrushna Adiecha <91903966+hk2cmpny@users.noreply.github.com>
2022-06-01 23:45:53 +0530

    lumen route support added

--- src/routes.php
+++ src/routes.php
@@ -3,27 +3,61 @@
 use Adiechahk\PaymentBackend\Controllers\PaymentController;
 use Illuminate\Support\Facades\Route;
 
-Route::withoutMiddleware([
-    \App\Http\Middleware\VerifyCsrfToken::class
-])->group(function() {
-    Route::get(
+if (app() instanceof \Illuminate\Foundation\Application) {
+    Route::withoutMiddleware([
+        \App\Http\Middleware\VerifyCsrfToken::class
+    ])->group(function() {
+        Route::get(
+            '/payment/instances',
+            [PaymentController::class, 'index']
+        )->name('payment.get');
+        Route::patch(
+            '/payment/instances/{paymentInstance}/ack',
+            [PaymentController::class, 'ack']
+        )->name('payment.ack');
+        Route::post(
+            '/payment/request',
+            [PaymentController::class, 'initPayment']
+        )->name('payment.init');
+        Route::any(
+            '/payment/callback',
+            [PaymentController::class, 'callback']
+        )->name('payment.callback');
+        Route::any(
+            '/payment/webhook',
+            [PaymentController::class, 'webhook']
+        )->name('payment.webhook');
+    });
+} else {
+    $this->app->router->get(
         '/payment/instances',
-        [PaymentController::class, 'index']
-    )->name('payment.get');
-    Route::patch(
+        ["as" => 'payment.get', 'PaymentController@index']
+    );
+    $this->app->router->patch(
         '/payment/instances/{paymentInstance}/ack',
-        [PaymentController::class, 'ack']
-    )->name('payment.ack');
-    Route::post(
+        ["as" => 'payment.ack', 'PaymentController@ack']
+    );
+    $this->app->router->post(
         '/payment/request',
-        [PaymentController::class, 'initPayment']
-    )->name('payment.init');
-    Route::any(
+        ["as" => 'payment.init', 'PaymentController@initPayment']
+    );
+    $this->app->router->get(
         '/payment/callback',
-        [PaymentController::class, 'callback']
-    )->name('payment.callback');
-    Route::any(
+        ["as" => 'payment.callback', 'PaymentController@callback']
+    );
+    $this->app->router->post(
+        '/payment/callback',
+        ["as" => 'payment.callback', 'PaymentController@callback']
+    );
+    $this->app->router->get(
         '/payment/webhook',
-        [PaymentController::class, 'webhook']
-    )->name('payment.webhook');
-});
+        ["as" => 'payment.webhook', 'PaymentController@webhook']
+    );    
+    $this->app->router->post(
+        '/payment/webhook',
+        ["as" => 'payment.webhook', 'PaymentController@webhook']
+    );    
+}
+
+
+
