f8145a43790f3089bcafe94944308fd0009d162e
Vaibhavi <vaibhavi@enstead.com>
2023-11-20 18:16:26 +0530

    Suggested change

--- src/V1/Controllers/CardController.php
+++ src/V1/Controllers/CardController.php
@@ -19,22 +19,9 @@ class CardController extends CardControllerDefault
             // Creating payment object and card for that.
             $payment = new Payment();
             return $payment->createCardByToken($card);            
-        }else{
-           
-            // Split month and year from expiry
-            $monthYearArray = explode('/', $request->expiry_date);
-
-            // Generate Token for creating card on stripe
-            $card = [
-                'number' => $request->number,
-                'exp_month' => $monthYearArray[0],
-                'exp_year' => $monthYearArray[1],
-                'cvc' => $request->cvc,
-            ];
-
-            // Creating payment object and card for that.
-            $payment = new Payment();
-            return $payment->createCard($card);
+        }else{      
+            // Use old method as it is  
+            return parent::create($request);
         }
         
     }
